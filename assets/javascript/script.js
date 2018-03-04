251008d374325f42e82154099e64c3c2721ac649
Alex Harris <akharri8@ncsu.edu>
2018-03-04 18:59:00 -0500

    Added HTTP request and event listener

---
+++ assets/javascript/script.js
@@ -0,0 +1,43 @@
+// Need to reach out to API to get a random Quote
+// Give the user the option to save the quote
+// If the user saves a quote, we can display it somewhere on the page
+// Use Fire base database for persisting data. 
+
+//http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1
+
+const quoteButton = document.getElementById('quoteButton');
+const quoteContent = document.getElementById('quoteContent');
+const authorContent = document.getElementById('authorContent');
+quoteButton.addEventListener('click', makeRequest);
+
+function makeRequest () {
+    var myRequest = new XMLHttpRequest()
+    method = "GET",
+    url = "https://random-quote-generator.herokuapp.com/api/quotes/",
+
+    myRequest.onreadystatechange = function requestHandler () {
+        if(myRequest.readyState === XMLHttpRequest.DONE && myRequest.status === 200) {
+            const quotesList = JSON.parse(myRequest.responseText);
+            const randomNumber = Math.floor(Math.random() * quotesList.length);
+            console.log(quotesList);
+            const quote = quotesList[randomNumber].quote;
+            const author = quotesList[randomNumber].author;
+            console.log(author);
+            quoteContent.textContent = quote;
+            authorContent.textContent = `- ${author}`;
+
+        } 
+      }
+
+    myRequest.open(method, url, true);
+    myRequest.send();
+}
+
+
+
+
+
+
+
+
+
