a49682a2476b6a7da88fe85ec6e0a2eccb66ac98
Alex Harris <akharri8@ncsu.edu>
2018-03-04 19:39:39 -0500

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,8 +2,6 @@
 // If the user saves a quote, we can display it somewhere on the page
 // Use Fire base database for persisting data. 
 
-//http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1
-
 const quoteButton = document.getElementById('quoteButton');
 const quoteContent = document.getElementById('quoteContent');
 const authorContent = document.getElementById('authorContent');
@@ -18,10 +16,8 @@ function makeRequest () {
         if(myRequest.readyState === XMLHttpRequest.DONE && myRequest.status === 200) {
             const quotesList = JSON.parse(myRequest.responseText);
             const randomNumber = Math.floor(Math.random() * quotesList.length);
-            console.log(quotesList);
             const quote = quotesList[randomNumber].quote;
             const author = quotesList[randomNumber].author;
-            console.log(author);
             quoteContent.textContent = quote;
             authorContent.textContent = `- ${author}`;
 
